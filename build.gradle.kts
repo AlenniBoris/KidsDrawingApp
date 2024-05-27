d0a5b3abd3c7fbd5f6d74fe9c5417cf4818dbe70
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2024-05-27 14:03:31 +0300

    finished kids drawing app

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,18 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
+buildscript{
+
+    repositories{
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+
+}
